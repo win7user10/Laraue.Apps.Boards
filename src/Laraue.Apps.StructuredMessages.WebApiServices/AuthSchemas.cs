e2c2f78538a4a9d04ba7f9cd6d1e495d21e0e065
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2026-04-21 17:27:17 +0300

    add missing AuthSchemas.cs

---
+++ src/Laraue.Apps.StructuredMessages.WebApiServices/AuthSchemas.cs
@@ -0,0 +1,7 @@
+﻿namespace Laraue.Apps.StructuredMessages.WebApiServices;
+
+public static class AuthSchemas
+{
+    public const string User = "User";
+    public const string Organization = "Organization";
+}
